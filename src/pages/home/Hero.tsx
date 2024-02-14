dd7c712570f82e9905c488052fb8b47a24d975da
Desgue <guedesgomero@gmail.com>
2024-02-14 23:52:04 +0000

    refactor: fix initial state in hero component

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -76,8 +76,8 @@ const Hero = () => {
 
 const Header = () => {
   const roles = ["Hey, I'm Eduardo Guedes ; )", "I'm a Software Developer" , "I'm a Hackerman 🕶️", "I'm Learning Game Dev"]
-  const [role, setRole] = React.useState("a Software Developer")
-  const [idx, setIdx] = React.useState(0)
+  const [role, setRole] = React.useState("Hey, I'm Eduardo Guedes ;")
+  const [idx, setIdx] = React.useState(1)
   React.useEffect(() => {
     let time = (role === "Hey, I'm Eduardo Guedes ; )") ? 3500 : 2000
     const interval = setInterval(() => {
